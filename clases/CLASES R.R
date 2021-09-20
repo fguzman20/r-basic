4f44a14ea82f6d52ebf7d5cd451b00f5ccd46fd0
fguzman20 <fguzman20@hotmail.com>
2021-09-20 09:53:21 -0600

    cambios2

---
+++ clases/CLASES R.R
@@ -0,0 +1,62 @@
+
+sqrt(25)
+exp(1)
+#mate discreta
+factorial(3)
+choose (5,3)
+#Logaritmo
+6^ log(4,6)
+
+log(8,2)
+
+#funciones trigonometricas
+cos(60*pi/180)
+?sin
+
+#FUNCIONES DE REDONDEO E IMPRESION
+print(sqrt(2),10) #10 cantidad de decimales SOLO PARA MOSTRAR
+round(sqrt(2),2) #2 cantidad de decimales  PARA TRABAJAR
+floor(sqrt(2)) #cuadra a valor entero
+ceiling(sqrt(2)) #un valor arriba
+round(digits = 1,5.50005)
+
+#VARIABLES 
+x = (pi^2)/2
+x= round(x,2) 
+x
+y<- 1.36
+y
+8.95 -> z
+x+y+z
+edad= 30
+nombre= 'marcos'
+
+#FUNCIONES
+#nombre_funcion = function(variable){cuerpo funcion}
+
+a = 8
+cuadrado = function(a){
+  a ^ 2 + (a * 3)
+}
+cuadrado(a)
+
+ls() MUESTRA OBJETOS
+rm(X)#ELIMINA OBJETOS
+
+
+opbasic = function(a,b){
+  print(a+b)
+  print("resta")
+  print(paste(sprintf("%i - %i = ",b,a),b-a))
+  print(a%/%b)
+  print(a*b)
+}
+
+opbasic(5,2)
+
+
+#NUMEROS COMPLEJOS:
+(3+2i)
+(3+2i)*5
+sqrt(3+2i)
+
